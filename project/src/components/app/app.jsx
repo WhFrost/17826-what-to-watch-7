4662f0ff8a1109733e9d8bec62497d761a96364a
Wh_Frost <whfrost63@gmail.com>
2021-06-10 12:46:17 +0400

    Добавит маршрутизацию

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPage from '../pages/main';
 
 function App(props) {
   const {film} = props;
-  return <MainPage film = {film}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainPage film = {film}/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
