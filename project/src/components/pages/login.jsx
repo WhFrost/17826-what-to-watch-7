53b1ee8cfa10b02a3cd283472f25eb09bf5d384b
Wh_Frost <whfrost63@gmail.com>
2021-07-19 13:31:20 +0400

    Добавит возможность аутентификации. WIP

--- project/src/components/pages/login.jsx
+++ project/src/components/pages/login.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {useHistory} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login} from '../../store/api-action';
+import {AppRoute} from '../../const';
 import HeaderLogo from '../logo/header-logo';
 
-function Login() {
+function Login(props) {
+  const {onSubmit} = props;
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  const history = useHistory();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    onSubmit({
+      login: loginRef.current.value,
+      password: passwordRef.current.value,
+    });
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -10,19 +30,43 @@ function Login() {
       </header>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form
+          action="#"
+          className="sign-in__form"
+          onSubmit={handleSubmit}
+        >
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <input
+                ref={loginRef}
+                className="sign-in__input"
+                type="email"
+                placeholder="Email address"
+                name="user-email"
+                id="user-email"
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+              <input
+                ref={passwordRef}
+                className="sign-in__input"
+                type="password"
+                placeholder="Password"
+                name="user-password"
+                id="user-password"
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
           <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
+            <button
+              onClick={() => history.push(AppRoute.ROOT)}
+              className="sign-in__btn"
+              type="submit"
+            >
+              Sign in
+            </button>
           </div>
         </form>
       </div>
@@ -30,4 +74,15 @@ function Login() {
   );
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  },
+});
+
+Login.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export {Login};
+export default connect(null, mapDispatchToProps)(Login);
